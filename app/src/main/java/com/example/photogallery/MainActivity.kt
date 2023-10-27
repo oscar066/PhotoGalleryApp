28ab9ce9b19891f001a4bb49db9d815c022c2b7b
oscar066 <oscarkaruga1@gmail.com>
2023-10-27 07:59:35 +0300

    :construction: Added and Wired up permission for new photos from photoGallery

--- app/src/main/java/com/example/photogallery/MainActivity.kt
+++ app/src/main/java/com/example/photogallery/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.photogallery
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.appbar.MaterialToolbar
@@ -11,5 +13,11 @@ class MainActivity : AppCompatActivity() {
 
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+    }
+
+    companion object {
+        fun newIntent(context: Context) : Intent {
+            return Intent(context, MainActivity::class.java)
+        }
     }
 }
