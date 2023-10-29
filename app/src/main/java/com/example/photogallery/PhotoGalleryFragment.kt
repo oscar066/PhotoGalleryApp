ba088e32efe7853d9ae20e1b4cdbbf3c30845cdc
oscar066 <oscarkaruga1@gmail.com>
2023-10-29 12:59:14 +0300

    :sparkles: Added Onclicklistener on the images completion of V2

--- app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
@@ -1,5 +1,6 @@
 package com.example.photogallery
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -61,7 +62,13 @@ class PhotoGalleryFragment : Fragment() {
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 photoGalleryViewModel.uiState.collect { state ->
-                    binding.photoGrid.adapter = PhotoListAdapter(state.images)
+                    binding.photoGrid.adapter = PhotoListAdapter(
+                        state.images
+                    ) {
+                        photoPageUri ->
+                        val intent = Intent(Intent.ACTION_VIEW, photoPageUri)
+                        startActivity(intent)
+                    }
                     searchView?.setQuery(state.query, false)
                     updatePollingState(state.isPolling)
                 }
--- app/src/main/java/com/example/photogallery/PhotoListAdapter.kt
+++ app/src/main/java/com/example/photogallery/PhotoListAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.photogallery
 
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -10,13 +11,16 @@ import com.example.photogallery.databinding.ListItemGalleryBinding
 class PhotoViewHolder (
     private val binding: ListItemGalleryBinding
 ) : RecyclerView.ViewHolder(binding.root) {
-    fun bind (galleryItem: GalleryItem) {
+    fun bind (galleryItem: GalleryItem, onItemClicked: (Uri) -> Unit) {
         binding.itemImageView.load(galleryItem.url)
+
+        binding.root.setOnClickListener { onItemClicked(galleryItem.photoPageUri)}
     }
 }
 
 class PhotoListAdapter (
-    private val galleryItems: List<GalleryItem>
+    private val galleryItems: List<GalleryItem>,
+    private val onItemClicked: (Uri) -> Unit
 ) : RecyclerView.Adapter<PhotoViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -26,7 +30,7 @@ class PhotoListAdapter (
 
     override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
         val item = galleryItems[position]
-        holder.bind(item)
+        holder.bind(item, onItemClicked)
     }
 
     override fun getItemCount() = galleryItems.size
